bae4fffbb02df14e0bc8ea3a75a32c8b6f56fdae
lpp <geolifestudy@gmail.com>
2026-07-03 13:11:36 +0800

    chore(scripts): 优化docker-compose启停脚本的项目名称获取方式
    将硬编码的项目名改为自动获取当前工作目录名称，提升脚本灵活性和可复用性

--- RAG/source/tools/docker_compose_template/docker_portainer_20260703/start.sh
+++ RAG/source/tools/docker_compose_template/docker_portainer_20260703/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-PROJECT_NAME=docker_portainer_20260703
+# PROJECT_NAME=docker_portainer_20260703
+# 方案一：获取【当前工作目录】的名称（推荐，若脚本与 compose 文件同级）
+PROJECT_NAME=$(basename "$PWD")
 COMPOSE_FILE=docker-compose.yaml
 PROJECT_NAME=$PROJECT_NAME docker compose -f $COMPOSE_FILE up -d
 # sed -i 's/\r$//' start.sh
--- RAG/source/tools/docker_compose_template/docker_portainer_20260703/stop.sh
+++ RAG/source/tools/docker_compose_template/docker_portainer_20260703/stop.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-PROJECT_NAME=docker_portainer_20260703
+# 方案一：获取【当前工作目录】的名称（推荐，若脚本与 compose 文件同级）
+PROJECT_NAME=$(basename "$PWD")
 COMPOSE_FILE=docker-compose.yaml
 # 启动 Docker Compose 服务（后台运行）docker compose down
 PROJECT_NAME=$PROJECT_NAME docker compose -f $COMPOSE_FILE down
